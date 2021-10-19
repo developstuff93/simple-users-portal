33d175a663a29ac803e34d0861974354b8db9c5e
Jose Arias <developstuff93@gmail.com>
2021-10-18 22:38:48 -0400

    add Patient Portal features

--- components/patient-portal.tsx
+++ components/patient-portal.tsx
@@ -1,20 +1,122 @@
 import * as React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form, Alert, Table, Container } from "react-bootstrap";
+import useAppState from "../hooks/useAppState";
+import BaseModal from "./base-modal";
 
 interface Props {
   closePortal(): void;
 }
 
+function AddPatientModal({ showAddPatient, setShowAddPatient }) {
+  const { addPatient } = useAppState();
+  const [error, setError] = React.useState("");
+
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [manager, setManager] = React.useState(0);
+
+  const handleCloseAddPatient = () => {
+    setShowAddPatient(false);
+  };
+
+  const handleAddPatient = () => {
+    if (!firstName.length || !lastName.length || isNaN(manager)) {
+      setError("All fields are reuiqred");
+      return;
+    }
+    setError("");
+    addPatient({ firstName, lastName, manager });
+    handleCloseAddPatient();
+  };
+
+  return (
+    <BaseModal
+      isOpen={showAddPatient}
+      title="Add User"
+      onClose={handleCloseAddPatient}
+      onConfirm={handleAddPatient}
+    >
+      <Form>
+        <Form.Group className="mt-2">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            type="text"
+            required
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mt-2">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type="text"
+            required
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Form.Group>
+        {error && (
+          <Alert className="mt-2" variant="danger">
+            {error}
+          </Alert>
+        )}
+      </Form>
+    </BaseModal>
+  );
+}
+
 export function PatientPortal({ closePortal }: Props) {
+  const { patients, users } = useAppState();
+  const [showAddPatient, setShowAddPatient] = React.useState(Boolean);
+
+  const userMap = {};
+  users.forEach((user) => {
+    userMap[user.id] = `${user.firstName} ${user.lastName}`;
+  });
+
+  const handleOpenAddPatientModal = () => {
+    setShowAddPatient(true);
+  };
+
   return (
     <Card>
       <Card.Body>
         <Card.Title>Patient Portal</Card.Title>
-        <Card.Text>Part 2 tasks will appear on this page</Card.Text>
-        <Button variant="secondary" onClick={() => closePortal()}>
-          Go back
-        </Button>
+        <Table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Manager</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patients.map((patient) => (
+              <tr key={patient.id}>
+                <td>{patient.firstName}</td>
+                <td>{patient.lastName}</td>
+                <td>{userMap[patient.manager]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Container>
+          <Button variant="primary" onClick={handleOpenAddPatientModal}>
+            Add User
+          </Button>
+          <Button
+            className="mx-2"
+            variant="secondary"
+            onClick={() => closePortal()}
+          >
+            Go back
+          </Button>
+        </Container>
       </Card.Body>
+      <AddPatientModal
+        showAddPatient={showAddPatient}
+        setShowAddPatient={setShowAddPatient}
+      />
     </Card>
   );
 }
