b3cac6485d31dda34a017993cd7854c28b5245eb
Jose Arias <developstuff93@gmail.com>
2021-10-20 09:14:48 -0400

    update title

--- components/nav.tsx
+++ components/nav.tsx
@@ -9,11 +9,13 @@ export function CourierNav({ selectPortal }: Props) {
   return (
     <Navbar bg="light" expand="lg" className="mb-5">
       <Container>
-        <Navbar.Brand href="#home">Courier Health Assignment</Navbar.Brand>
+        <Navbar.Brand href="#home">Users Management</Navbar.Brand>
         <Navbar.Collapse role="navigation">
           <Nav className="me-auto">
             <Nav.Link onClick={() => selectPortal(undefined)}>Home</Nav.Link>
-            <Nav.Link onClick={() => selectPortal(1)}>User Creation Portal</Nav.Link>
+            <Nav.Link onClick={() => selectPortal(1)}>
+              User Creation Portal
+            </Nav.Link>
             <Nav.Link onClick={() => selectPortal(2)}>Patient Portal</Nav.Link>
           </Nav>
         </Navbar.Collapse>
